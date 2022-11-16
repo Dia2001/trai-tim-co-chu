241e296e95aa842bee15e01533687c102b79afe5
nguyenvandiatlvn3@gmail.com <nguyenvandiatlvn3@gmail.com>
2022-11-16 16:16:46 +0700

    Add project files.

---
+++ trai-tim-co-chu/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+int count = 0;
+for (float y = 3f; y > -2f; y -= 0.1f)
+{
+    for (float x = -2.5f; x < 2.5f; x += 0.04f)
+    {
+        float a = x * x + y * y - 4f;
+        if ((a * a * a - x * x * y * y * y) < -0.0f)
+        {
+            String str = " ANH YÊU EM NHIỀU ";
+            int num = count % str.Length;
+            Console.Write(str[num]);
+            count++;
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+    Thread.Sleep(100);
+}
+Console.WriteLine("Dìa văn IT , đại học Quy Nhơn ");
+Console.ReadLine();
